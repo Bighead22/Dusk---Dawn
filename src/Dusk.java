3b6dd3d4f1a52282f30f75473fc59270368fcdba
Bighead22 <cashstein1@icloud.com>
2026-04-02 15:29:37 -0700

    add dusk

---
+++ src/Dusk.java
@@ -0,0 +1,7 @@
+public class Dusk extends Player{
+
+    public Dusk(int x, int y, int speed, int health) {
+        super(x, y, 9, 19, "assets/DawnAssets/DawnI.png", speed, health);
+    }
+    
+}
